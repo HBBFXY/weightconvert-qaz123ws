51ddfb188e51fc71f59f90a3f838793ca897aae6
qaz123ws <977017719@qq.com>
2025-09-11 21:19:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-#获取用户输入的重量及单位
-weight_input=input()
-#提取数值部分（转换为浮点数）和单位部分
-num=float(weight_input[:-2])
-unit=weight_input[-2:]
-#定义转换系数
-kg_to_pd=2.2046
-if unit=="kg":
-  #千克转磅
-  result=num*kg_to_pd
-  print(f"对应的英雄重量为
-  {result:.3f}磅")
-elif unt=="pd":
-  #磅转千克
-  result=num/kg_to_pd
-  print(f"对应的公制重量为
-  {result:.3f}公斤")
+weight = input("请输入要转换的重量")
+if(weight[-1] == 'g'):
+  exweight = (int(weight[0:-2]))*2.2046
+  print("对应的英制重量为%.3f公斤"%(exweight))
+if(weight[-1] == 'd'):
+  exweight1 = (int(weight[0:-2]))/2.2046
+  print("对应的英制重量为%.3f公斤"%(exweight1))
